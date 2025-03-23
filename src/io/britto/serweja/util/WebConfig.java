4ec5a6d70470d4748a6eceb2498be2fa66347b71
FabioBritto <fabio.tritono@gmail.com>
2025-03-23 13:39:51 -0300

    Arrumando extensão do HashMap content

--- src/io/britto/serweja/util/WebConfig.java
+++ src/io/britto/serweja/util/WebConfig.java
@@ -7,12 +7,12 @@ public class WebConfig {
 	public static final String DOCUMENT_ROOT="/Users/Samsung/Documents/SerWeJa";
 	
 	public static HashMap<String, String> content = new HashMap<String, String>() {{
-		put("html", "text/html");
-		put("htm", "text/html");
-		put("jpg", "image/jpg");
-		put("png", "image/png");
-		put("jpeg", "image/jpeg");
-		put("txt", "text/plain");
+		put(".html", "text/html");
+		put(".htm", "text/html");
+		put(".jpg", "image/jpg");
+		put(".png", "image/png");
+		put(".jpeg", "image/jpeg");
+		put(".txt", "text/plain");
 		
 	}};
 }
