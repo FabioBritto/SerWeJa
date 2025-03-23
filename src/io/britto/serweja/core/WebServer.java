4de98807193dec1c0320deb55c3b344de1d5c49b
FabioBritto <fabio.tritono@gmail.com>
2025-03-23 12:09:13 -0300

    Capturando valores de httpMethod e resourcePath

--- src/io/britto/serweja/core/WebServer.java
+++ src/io/britto/serweja/core/WebServer.java
@@ -1,6 +1,8 @@
 package io.britto.serweja.core;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,10 +10,14 @@ public class WebServer {
 
 	private ServerSocket serverSocket;
 	
+	private String httpMethod;
+	private String resourcePath;
+	
 	public WebServer(int port) {
 		try {
 			this.serverSocket = new ServerSocket(port);
 			System.out.println("SerWeJa started on Port: " + port);
+			System.out.println("Waiting for Connections...");
 		}
 		catch(IOException ex) {
 			System.out.println("Could not initialize SerWeJa on Port: " + port);
@@ -28,7 +34,7 @@ public class WebServer {
 				 */
 				Socket socket = serverSocket.accept(); //Aceito trocar dados com o cliente
 				handleRequest(socket);
-				socket.close();
+				
 			}
 			catch(IOException ex) {
 				System.out.println("Couldn't handle client request");
@@ -42,6 +48,28 @@ public class WebServer {
 	
 	
 	private void handleRequest(Socket socket) {
-		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String line = null;
+			do {
+				line = br.readLine();
+				if(line.startsWith("GET") || line.startsWith("POST")) {
+					httpMethod = line.substring(0,line.indexOf(" "));
+					resourcePath = line.substring(line.indexOf(" "), line.lastIndexOf(" "));
+					
+					handleOutput(httpMethod, resourcePath);
+				}
+				//System.out.println("DEBUG - " + line);
+			}while(!line.isBlank());
+			socket.close();
+		}
+		catch(IOException e) {
+			System.err.println("Error on Handle Request");
+		}
+	}
+	
+	private void handleOutput(String httpMethod, String resourcePath) {
+		System.out.println("HTTP Methdo: " + httpMethod);
+		System.out.println("Resource Path: " + resourcePath);
 	}
 }
